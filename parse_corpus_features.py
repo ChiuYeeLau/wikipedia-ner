8e5474104753dd515c0b4a64bd3d8fb424f41b04
Cristian Cardellino <crscardellino@gmail.com>
2016-06-04 00:03:07 -0300

    Features parser

---
+++ parse_corpus_features.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+import argparse
+import cPickle
+import os
+import sys
+from tqdm import tqdm
+from wikipedianer.corpus.parser import FeatureExtractor, WikipediaCorpusColumnParser
+
+FILES_SENTENCES = {
+    "doc_01": 5255631,
+    "doc_02": 2584456,
+    "doc_03": 2089800,
+    "doc_04": 1762920,
+    "doc_05": 1605431,
+    "doc_06": 1688569,
+    "doc_07": 1813089,
+    "doc_08": 1841952,
+    "doc_09": 1495951,
+    "doc_10": 514936
+}
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_dir", type=unicode)
+    parser.add_argument("output_dir", type=unicode)
+    parser.add_argument("--stopwords", type=bool, action="store_true")
+
+    args = parser.parse_args()
+
+    feature_extractor = FeatureExtractor(
+        max_ngram_length=6,
+        disjunctive_left_window=4,
+        disjunctive_right_window=4,
+        tag_sequence_window=2
+    )
+    gazetteer = set()
+
+    for conll_file in sorted(os.listdir(args.input_dir)):
+        corpus_doc, _ = conll_file.split(".", 1)
+
+        print('Collecting features for corpus {}'.format(corpus_doc), file=sys.stderr)
+
+        parser = WikipediaCorpusColumnParser(os.path.join(args.input_dir, conll_file), args.stopwords)
+
+        for sentence in tqdm(parser, total=FILES_SENTENCES[corpus_doc]):
+            if sentence.has_named_entity:
+                feature_extractor.features_from_sentence(sentence)
+                gazetteer.update(sentence.get_gazettes())
+
+    print('Updating features with gazette features', file=sys.stderr)
+    feature_extractor.update_features({'gazette:{}'.format(gazette for gazette in gazetteer)})
+
+    print('Saving sorted features', file=sys.stderr)
+    feature_extractor.save_sorted_features(os.path.join(args.output_dir, 'sorted_features.pickle'))
+
+    print('Saving gazetteer', file=sys.stderr)
+    with open(os.path.join(args.output_dir, 'gazetteer.pickle', 'wb')) as f:
+        cPickle.dump(gazetteer, f)
+
+    print('All operations finished', file=sys.stderr)
--- wikipedianer/corpus/parser.py
+++ wikipedianer/corpus/parser.py
@@ -2,99 +2,166 @@
 
 from __future__ import absolute_import, unicode_literals
 
+import cPickle
+from nltk.corpus import stopwords
 from .base import Sentence, Word, WORDNET_CATEGORIES, YAGO_RELATIONS
 
 
-class InstanceExtractor(object):
+STOPWORDS_SET = set(stopwords.words())
+
+
+# class InstanceExtractor(object):
+#     def __init__(self, **kwargs):
+#         self.token = bool(kwargs.get('token', False))
+#         self.current_tag = bool(kwargs.get('current_tag', False))
+#         self.affixes = bool(kwargs.get('affixes', False))
+#         self.max_ngram_lenght = int(kwargs.get('max_ngram_length', 6))
+#         self.prev_token = bool(kwargs.get('prev_token', False))
+#         self.next_token = bool(kwargs.get('next_token', False))
+#         self.disjunctive_left_window = int(kwargs.get('disjunctive_left_window', 0))
+#         self.disjunctive_right_window = int(kwargs.get('disjunctive_right_window', 0))
+#         self.tag_sequence_window = int(kwargs.get('tag_sequence_window', 0))
+#         self.clean_gazetteer = kwargs.get('clean_gazette', {})
+#         self.sloppy_gazetteer = kwargs.get('sloppy_gazette', {})
+#
+#     def _features_for_word(self, word, sentence, named_entity=None):
+#         """
+#         :param word: wikipedianer.corpus.base.Word
+#         :param sentence: wikipedianer.corpus.base.Sentence
+#         :param named_entity: wikipedianer.corpus.base.NamedEntity
+#         """
+#         features = {}
+#
+#         if self.token:
+#             features['token:current'] = word.token
+#
+#         if self.current_tag:
+#             features['tag:current'] = word.tag
+#
+#         if self.affixes:
+#             prefixes, suffixes = word.get_affixes(self.max_ngram_lenght)
+#
+#             for i in range(len(prefixes)):
+#                 prefix = prefixes[i]
+#                 features['prefix:{}-gram'.format(len(prefix))] = prefix
+#
+#             for i in range(len(suffixes)):
+#                 suffix = suffixes[i]
+#                 features['suffix:{}-gram'.format(len(suffix))] = suffix
+#
+#         if self.prev_token and word.idx > 0:
+#             features['token:prev'] = sentence[word.idx-1].token
+#
+#         if self.next_token and word.idx < len(sentence) - 1:
+#             features['token:next'] = sentence[word.idx+1].token
+#
+#         for wrd in sentence.get_left_window(word.idx, self.disjunctive_left_window):
+#             features['left:{}'.format(wrd.token)] = 1
+#
+#         for wrd in sentence.get_right_window(word.idx, self.disjunctive_right_window):
+#             features['right:{}'.format(wrd.token)] = 1
+#
+#         if self.tag_sequence_window > 0:
+#             features['tag:surrounding:sequence'] = ' '.join([
+#                 wrd.tag for wrd in
+#                 sentence.get_left_window(word.idx, self.tag_sequence_window) +
+#                 sentence.get_right_window(word.idx, self.tag_sequence_window)
+#             ])
+#
+#         if named_entity is not None and named_entity.entity_gazette in self.clean_gazetteer:
+#             for feature in self.clean_gazetteer[named_entity.entity_gazette]:
+#                 features[feature] = 1
+#
+#         if word.token in self.sloppy_gazetteer:
+#             for feature in self.sloppy_gazetteer[word.token]:
+#                 features[feature] = 1
+#
+#         return features
+#
+#     def get_instances_for_sentence(self, sentence):
+#         """
+#         :param sentence: wikipedianer.corpus.base.Sentence
+#         """
+#         instances = []
+#         labels = []
+#
+#         for unit in sentence.get_words_and_entities():
+#             if unit.name == "Word":
+#                 instances.append(self._features_for_word(unit, sentence))
+#                 labels.append(unit.short_label)
+#             else:
+#                 for word in unit:
+#                     instances.append(self._features_for_word(word, sentence, unit))
+#                     labels.append(word.short_label)
+#
+#         return instances, labels
+
+
+class FeatureExtractor(object):
     def __init__(self, **kwargs):
-        self.token = bool(kwargs.get('token', False))
-        self.current_tag = bool(kwargs.get('current_tag', False))
-        self.affixes = bool(kwargs.get('affixes', False))
-        self.max_ngram_lenght = int(kwargs.get('max_ngram_length', 6))
-        self.prev_token = bool(kwargs.get('prev_token', False))
-        self.next_token = bool(kwargs.get('next_token', False))
+        self.features_set = {'in:clean:gazetteer', 'in_sloppy_gazetteer'}
+        self.max_ngram_length = kwargs.get('max_ngram_length', 0)
         self.disjunctive_left_window = int(kwargs.get('disjunctive_left_window', 0))
         self.disjunctive_right_window = int(kwargs.get('disjunctive_right_window', 0))
         self.tag_sequence_window = int(kwargs.get('tag_sequence_window', 0))
-        self.clean_gazetteer = kwargs.get('clean_gazette', {})
-        self.sloppy_gazetteer = kwargs.get('sloppy_gazette', {})
 
-    def _features_for_word(self, word, sentence, named_entity=None):
-        """
-        :param word: wikipedianer.corpus.base.Word
-        :param sentence: wikipedianer.corpus.base.Sentence
-        :param named_entity: wikipedianer.corpus.base.NamedEntity
-        """
-        features = {}
+    def _features_from_word(self, word, sentence):
+        self.features_set.add('token:current={}'.format(word.token))
+        self.features_set.add('tag:current={}'.format(word.tag))
 
-        if self.token:
-            features['token:current'] = word.token
+        prefixes, suffixes = word.get_affixes(self.max_ngram_length)
 
-        if self.current_tag:
-            features['tag:current'] = word.tag
+        for i in range(len(prefixes)):
+            prefix = prefixes[i]
+            self.features_set.add('prefix:{}-gram={}'.format(len(prefix), prefix))
 
-        if self.affixes:
-            prefixes, suffixes = word.get_affixes(self.max_ngram_lenght)
+        for i in range(len(suffixes)):
+            suffix = suffixes[i]
+            self.features_set.add('suffix:{}-gram={}'.format(len(suffix), suffix))
 
-            for i in range(len(prefixes)):
-                prefix = prefixes[i]
-                features['prefix:{}-gram'.format(len(prefix))] = prefix
+        if word.idx > 0:
+            self.features_set.add('token:prev={}'.format(sentence[word.idx-1].token))
 
-            for i in range(len(suffixes)):
-                suffix = suffixes[i]
-                features['suffix:{}-gram'.format(len(suffix))] = suffix
-
-        if self.prev_token and word.idx > 0:
-            features['token:prev'] = sentence[word.idx-1].token
-
-        if self.next_token and word.idx < len(sentence) - 1:
-            features['token:next'] = sentence[word.idx+1].token
+        if word.idx < len(sentence) - 1:
+            self.features_set.add('token:next={}'.format(sentence[word.idx+1].token))
 
         for wrd in sentence.get_left_window(word.idx, self.disjunctive_left_window):
-            features['left:{}'.format(wrd.token)] = 1
+            self.features_set.add('left:token={}'.format(wrd.token))
 
         for wrd in sentence.get_right_window(word.idx, self.disjunctive_right_window):
-            features['right:{}'.format(wrd.token)] = 1
+            self.features_set.add('right:token={}'.format(wrd.token))
 
         if self.tag_sequence_window > 0:
-            features['tag:surrounding:sequence'] = ' '.join([
+            self.features_set.add('tag:surrounding:sequence={}'.format('|'.join([
                 wrd.tag for wrd in
-                sentence.get_left_window(word.idx, self.tag_sequence_window) +
+                sentence.get_left_window(word.idx, self.tag_sequence_window) + [word] +
                 sentence.get_right_window(word.idx, self.tag_sequence_window)
-            ])
+            ])))
 
-        if named_entity is not None and named_entity.entity_gazette in self.clean_gazetteer:
-            for feature in self.clean_gazetteer[named_entity.entity_gazette]:
-                features[feature] = 1
-
-        if word.token in self.sloppy_gazetteer:
-            for feature in self.sloppy_gazetteer[word.token]:
-                features[feature] = 1
-
-        return features
-
-    def get_instances_for_sentence(self, sentence):
-        """
-        :param sentence: wikipedianer.corpus.base.Sentence
-        """
-        instances = []
-        labels = []
-
+    def features_from_sentence(self, sentence):
         for unit in sentence.get_words_and_entities():
             if unit.name == "Word":
-                instances.append(self._features_for_word(unit, sentence))
-                labels.append(unit.short_label)
+                self._features_from_word(unit, sentence)
             else:
                 for word in unit:
-                    instances.append(self._features_for_word(word, sentence, unit))
-                    labels.append(word.short_label)
+                    self._features_from_word(word, sentence)
 
-        return instances, labels
+    def update_features(self, features):
+        self.features_set.update(features)
+
+    @property
+    def sorted_features(self):
+        return {feature: idx for idx, feature in enumerate(sorted(self.features_set))}
+
+    def save_sorted_features(self, file_name):
+        with open(file_name, 'wb') as f:
+            cPickle.dump(self.sorted_features, f)
 
 
 class WikipediaCorpusColumnParser(object):
-    def __init__(self, file_path):
+    def __init__(self, file_path, remove_stop_words=False):
         self.file_path = file_path
+        self.remove_stop_words = remove_stop_words
 
     def __iter__(self):
         words = []
@@ -111,6 +178,10 @@ class WikipediaCorpusColumnParser(object):
                     yield s
                 else:
                     _, token, tag, class_string, head, dep = line.split()
+
+                    if self.remove_stop_words and token in STOPWORDS_SET:
+                        continue
+
                     widx = len(words)
 
                     if class_string.strip() != 'O':

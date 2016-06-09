5ac8deec165a954491c329e493403b0f9ac88b58
Cristian Cardellino <crscardellino@gmail.com>
2016-06-09 15:17:28 -0300

    Dataset splitter

---
+++ split_dataset.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+import argparse
+import cPickle as pickle
+import numpy as np
+import re
+import sys
+
+from collections import Counter
+from wikipedianer.dataset.preprocess import StratifiedSplitter
+
+
+def ner_label_replace(labels, mappings):
+    for label in labels:
+        if label.startswith('O'):
+            yield 'O'
+        else:
+            yield 'I'
+
+
+def ne_person_label_replace(labels, mappings):
+    for label in labels:
+        label = re.sub(r'^[BI]-', '', label)
+        if label == 'O':
+            yield 'O'
+        elif 'no_person' in mappings.get(label, set()):
+            yield 'no_person'
+        elif 'wordnet_person_100007846' in mappings.get(label, set()):
+            yield 'wordnet_person_100007846'
+
+
+def ne_category_label_replace(labels, mappings):
+    for label in labels:
+        label = re.sub(r'^[BI]-', '', label)
+        if label == 'O':
+            yield 'O'
+        elif 'wordnet_movie_106613686' in mappings.get(label, set()):
+            yield 'wordnet_movie_106613686'
+        elif 'wordnet_soundtrack_104262969' in mappings.get(label, set()):
+            yield 'wordnet_soundtrack_104262969'
+        elif 'wordnet_actor_109765278' in mappings.get(label, set()):
+            yield 'wordnet_actor_109765278'
+        elif 'wordnet_film_director_110088200' in mappings.get(label, set()):
+            yield 'wordnet_film_director_110088200'
+        elif 'wordnet_film_maker_110088390' in mappings.get(label, set()):
+            yield 'wordnet_film_maker_110088390'
+
+
+def ne_uri_label_replace(labels, mappings):
+    for label in labels:
+        yield re.sub(r"^B-", "I-", label)
+
+
+labels_replacements = [ner_label_replace, ne_person_label_replace, ne_category_label_replace, ne_uri_label_replace]
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("labels_path", type=unicode)
+    parser.add_argument("save_path", type=unicode)
+    parser.add_argument("mappings", type=unicode)
+    parser.add_argument("--train_size", type=float, default=0.8)
+    parser.add_argument("--test_size", type=float, default=0.1)
+    parser.add_argument("--validation_size", type=float, default=0.1)
+
+    args = parser.parse_args()
+
+    print('Loading labels from file {}'.format(args.labels_path), file=sys.stderr)
+    with open(args.labels_path, 'rb') as f:
+        labels = np.array(pickle.load(f))
+
+    with open(args.mappings, 'rb') as f:
+        class_mappings = pickle.load(f)
+
+    for idx, replacement_function in enumerate(labels_replacements, start=1):
+        print('Getting replaced labels for category {}'.format(idx), file=sys.stderr)
+
+        replaced_labels = list(replacement_function(labels, class_mappings))
+
+        filtered_classes = {l for l, v in Counter(replaced_labels).iteritems()
+                            if (args.validation_size > 0. and v >= 3) or v >= 2}
+        filtered_indices = np.array([i for i, l in enumerate(replaced_labels) if l in filtered_classes],
+                                    dtype=np.int32)
+
+        strat_split = StratifiedSplitter(np.array(replaced_labels), filtered_indices)
+
+        print('Splitting the dataset', file=sys.stderr)
+        strat_split.split_dataset(train_size=args.train_size, test_size=args.test_size,
+                                  validation_size=args.validation_size)
+
+        print('Saving splitted indices', file=sys.stderr)
+        strat_split.save_splitted_dataset_indices(args.save_path)

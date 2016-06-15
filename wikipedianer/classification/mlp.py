f667dc932ae0902a3e15390cbbbf9df54665078f
Cristian Cardellino <crscardellino@gmail.com>
2016-06-15 16:15:15 -0300

    Reporting training only last. Reporting loss as results also

--- wikipedianer/classification/mlp.py
+++ wikipedianer/classification/mlp.py
@@ -12,8 +12,9 @@ from .base import BaseClassifier
 
 
 class MultilayerPerceptron(BaseClassifier):
-    def __init__(self, dataset, labels, train_indices, test_indices, validation_indices, logs_dir, results_dir,
-                 layers, learning_rate=0.01, training_epochs=1500, batch_size=2000, loss_report=50):
+    def __init__(self, dataset, labels, train_indices, test_indices, validation_indices, saves_dir, results_dir,
+                 experiment_name, layers, learning_rate=0.01, training_epochs=1500, batch_size=2000, loss_report=50,
+                 pre_weights=None, pre_biases=None):
         super(MultilayerPerceptron, self).__init__(dataset, labels, train_indices, test_indices, validation_indices)
 
         assert batch_size <= self.train_dataset.shape[0]
@@ -25,18 +26,30 @@ class MultilayerPerceptron(BaseClassifier):
         self.train_offset = 0
 
         self.layers = [self.X]
+        self.weights = []
+        self.biases = []
 
         # Create the layers
         for layer_idx, (size_prev, size_current) in enumerate(zip([self.input_size] + layers, layers)):
             print('Creating hidden layer {}: {} -> {}'.format(layer_idx, size_prev, size_current), file=sys.stderr)
             with tf.name_scope('hidden_layer_{}'.format(layer_idx)):
-                weights = tf.Variable(
-                    tf.truncated_normal([size_prev, size_current],
-                                        stddev=1.0 / np.sqrt(size_prev)),
-                    name='weights'
-                )
-                biases = tf.Variable(tf.zeros([size_current]), name='biases')
+                if pre_weights and len(pre_weights) > layer_idx:
+                    weights = tf.Variable(pre_weights[layer_idx], name='weights')
+                else:
+                    weights = tf.Variable(
+                        tf.truncated_normal([size_prev, size_current],
+                                            stddev=1.0 / np.sqrt(size_prev)),
+                        name='weights'
+                    )
+
+                if pre_biases and len(pre_biases) > layer_idx:
+                    biases = tf.Variable(pre_biases[layer_idx])
+                else:
+                    biases = tf.Variable(tf.zeros([size_current]), name='biases')
+
                 layer = tf.nn.relu(tf.matmul(self.layers[-1], weights) + biases)
+                self.weights.append(weights)
+                self.biases.append(biases)
                 self.layers.append(layer)
 
         # The last layer is for the classifier
@@ -65,14 +78,12 @@ class MultilayerPerceptron(BaseClassifier):
 
         self.init = tf.initialize_all_variables()
 
-        # Tensorboard logs
-        self.logs_dir = logs_dir
-        self.summary_op = tf.merge_all_summaries()
-        self.summary_writer = None
-
-        # results
+        # results and saves
+        self.saves_dir = saves_dir
+        self.experiment_name = experiment_name
         self.results_dir = results_dir
         self.results = dict(
+            loss=[],
             train_accuracy=[],
             test_accuracy=[],
             validation_accuracy=[],
@@ -130,51 +141,52 @@ class MultilayerPerceptron(BaseClassifier):
     def _save_results(self):
         header = ','.join(self.classes).encode('utf-8')
 
+        # Loss
+        np.savetxt(os.path.join(self.results_dir, 'loss_{}.txt'.format(self.experiment_name)),
+                   np.array(self.results['loss'], dtype=np.float32),
+                   fmt='%.2f'.encode('utf-8'), delimiter=','.encode('utf-8'))
+
         # Train
-        np.savetxt(os.path.join(self.results_dir, 'train_accuracy_{}.txt'.format("_".join(self.layers))),
+        np.savetxt(os.path.join(self.results_dir, 'train_accuracy_{}.txt'.format(self.experiment_name)),
                    np.array(self.results['train_accuracy'], dtype=np.float32),
                    fmt='%.2f'.encode('utf-8'), delimiter=','.encode('utf-8'))
-        np.savetxt(os.path.join(self.results_dir, 'train_precision_{}.txt'.format("_".join(self.layers))),
+        np.savetxt(os.path.join(self.results_dir, 'train_precision_{}.txt'.format(self.experiment_name)),
                    np.array(self.results['train_precision'], dtype=np.float32),
                    fmt='%.2f'.encode('utf-8'), delimiter=','.encode('utf-8'),
                    header=header)
-        np.savetxt(os.path.join(self.results_dir, 'train_recall_{}.txt'.format("_".join(self.layers))),
+        np.savetxt(os.path.join(self.results_dir, 'train_recall_{}.txt'.format(self.experiment_name)),
                    np.array(self.results['train_recall'], dtype=np.float32),
                    fmt='%.2f'.encode('utf-8'), delimiter=','.encode('utf-8'),
                    header=header)
 
         # Test
-        np.savetxt(os.path.join(self.results_dir, 'test_accuracy_{}.txt'.format("_".join(self.layers))),
+        np.savetxt(os.path.join(self.results_dir, 'test_accuracy_{}.txt'.format(self.experiment_name)),
                    np.array(self.results['test_accuracy'], dtype=np.float32),
                    fmt='%.2f'.encode('utf-8'), delimiter=','.encode('utf-8'))
-        np.savetxt(os.path.join(self.results_dir, 'test_precision_{}.txt'.format("_".join(self.layers))),
+        np.savetxt(os.path.join(self.results_dir, 'test_precision_{}.txt'.format(self.experiment_name)),
                    np.array(self.results['test_precision'], dtype=np.float32),
                    fmt='%.2f'.encode('utf-8'), delimiter=','.encode('utf-8'),
                    header=header)
-        np.savetxt(os.path.join(self.results_dir, 'test_recall_{}.txt'.format("_".join(self.layers))),
+        np.savetxt(os.path.join(self.results_dir, 'test_recall_{}.txt'.format(self.experiment_name)),
                    np.array(self.results['test_recall'], dtype=np.float32),
                    fmt='%.2f'.encode('utf-8'), delimiter=','.encode('utf-8'),
                    header=header)
 
         # Validation
-        np.savetxt(os.path.join(self.results_dir, 'validation_accuracy_{}.txt'.format("_".join(self.layers))),
+        np.savetxt(os.path.join(self.results_dir, 'validation_accuracy_{}.txt'.format(self.experiment_name)),
                    np.array(self.results['validation_accuracy'], dtype=np.float32),
                    fmt='%.2f'.encode('utf-8'), delimiter=','.encode('utf-8'))
-        np.savetxt(os.path.join(self.results_dir, 'validation_precision_{}.txt'.format("_".join(self.layers))),
+        np.savetxt(os.path.join(self.results_dir, 'validation_precision_{}.txt'.format(self.experiment_name)),
                    np.array(self.results['validation_precision'], dtype=np.float32),
                    fmt='%.2f'.encode('utf-8'), delimiter=','.encode('utf-8'),
                    header=header)
-        np.savetxt(os.path.join(self.results_dir, 'validation_recall_{}.txt'.format("_".join(self.layers))),
+        np.savetxt(os.path.join(self.results_dir, 'validation_recall_{}.txt'.format(self.experiment_name)),
                    np.array(self.results['validation_recall'], dtype=np.float32),
                    fmt='%.2f'.encode('utf-8'), delimiter=','.encode('utf-8'),
                    header=header)
 
     def train(self):
         with tf.Session() as sess:
-            last_loss = np.inf
-
-            self.summary_writer = tf.train.SummaryWriter(self.logs_dir, sess.graph)
-
             sess.run(tf.initialize_all_variables())
 
             print('Training classifier', file=sys.stderr)
@@ -190,18 +202,9 @@ class MultilayerPerceptron(BaseClassifier):
 
                 if epoch % self.loss_report == 0:
                     print('\nEpoch {}: loss = {:.2f}'.format(epoch, loss), file=sys.stderr)
+                    self.results['loss'].append(loss)
 
-                    summary_str = sess.run(self.summary_op, feed_dict=feed_dict)
-                    self.summary_writer.add_summary(summary_str, epoch)
-                    self.summary_writer.flush()
-
-                if epoch != 0 and epoch % (self.training_epochs / 10) == 0:
-                    accuracy, precision, recall = self._evaluate(sess, self.train_dataset, self.train_labels,
-                                                                 'Train')
-                    print('Training accuracy: {:.2f}'.format(accuracy), file=sys.stderr)
-                    self._add_results('train', accuracy, precision, recall)
-
-                if epoch % (self.training_epochs / 20) == 0:
+                if epoch % (self.loss_report * 2) == 0:
                     accuracy, precision, recall = self._evaluate(sess, self.validation_dataset, self.validation_labels,
                                                                  'Validation')
                     print('Validation accuracy: {:.2f}'.format(accuracy), file=sys.stderr)
@@ -209,23 +212,19 @@ class MultilayerPerceptron(BaseClassifier):
 
                     if len(self.results['validation_accuracy']) >= 2:
                         delta_acc = self.results['validation_accuracy'][-2] - self.results['validation_accuracy'][-1]
-                        delta_loss = last_loss - loss
+                        delta_loss = self.results['loss'][-1] - loss
 
                         if delta_acc < 1e-3 and delta_loss < 1e-3:
                             print('Validation accuracy converging: ' +
                                   'delta_acc {:.2f} / delta_loss {:.2f}.' .format(delta_acc, delta_loss),
                                   file=sys.stderr)
-
-                            accuracy, precision, recall = self._evaluate(sess, self.train_dataset, self.train_labels,
-                                                                         'Train')
-                            print('Final training accuracy: {:.2f}'.format(accuracy), file=sys.stderr)
-                            self._add_results('train', accuracy, precision, recall)
-
                             break
 
-                    last_loss = loss
-
             print('Finished training', file=sys.stderr)
+
+            accuracy, precision, recall = self._evaluate(sess, self.train_dataset, self.train_labels, 'Train')
+            print('Training accuracy: {:.2f}'.format(accuracy), file=sys.stderr)
+            self._add_results('train', accuracy, precision, recall)
 
             accuracy, precision, recall = self._evaluate(sess, self.test_dataset, self.test_labels, 'Test')
             print('Testing accuracy: {:.2f}'.format(accuracy), file=sys.stderr)

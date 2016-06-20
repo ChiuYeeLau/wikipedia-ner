015c1986f4d06cb6d71f9ef20f7970a1d966e473
Cristian Cardellino <crscardellino@gmail.com>
2016-06-20 14:49:00 -0300

    collect gazetteer

--- collect_gazetteers.py
+++ collect_gazetteers.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 import cPickle
 import os
 import sys
+from collections import defaultdict
 from tqdm import tqdm
 from wikipedianer.corpus.parser import WikipediaCorpusColumnParser
 
@@ -23,36 +24,29 @@ if __name__ == "__main__":
     input_dir = sys.argv[1]
     output_dir = sys.argv[2]
 
+    gazetteer = defaultdict(int)
+
     for corpus_file in os.listdir(input_dir):
         corpus_doc, _ = corpus_file.split(".", 1)
         print('Getting gazettes for corpus {}'.format(corpus_doc), file=sys.stderr)
-
-        clean_gazettes = {}
-        sloppy_gazettes = {}
 
         parser = WikipediaCorpusColumnParser(os.path.join(input_dir, corpus_file), remove_stop_words=True)
 
         for sentence in tqdm(parser, total=FILES_SENTENCES[corpus_file]):
             if sentence.has_named_entity:
-                sentence_clean_gazettes = sentence.get_clean_gazettes()
-                sentence_sloppy_gazettes = sentence.get_sloppy_gazettes()
+                for gazette, value in sentence.get_gazettes().iteritems():
+                    gazetteer[gazette] += value
 
-                for clean_gazette in sentence_clean_gazettes:
-                    if clean_gazette in clean_gazettes:
-                        clean_gazettes[clean_gazette].update(sentence_clean_gazettes[clean_gazette])
-                    else:
-                        clean_gazettes[clean_gazette] = sentence_clean_gazettes[clean_gazette]
+    print('Saving gazetteer', file=sys.stderr)
+    with open(os.path.join(output_dir, 'gazetteer.pickle'), 'wb') as f:
+        cPickle.dump(gazetteer, f)
 
-                for sloppy_gazette in sentence_sloppy_gazettes:
-                    if sloppy_gazette in sloppy_gazettes:
-                        sloppy_gazettes[sloppy_gazette].update(sentence_sloppy_gazettes[sloppy_gazette])
-                    else:
-                        sloppy_gazettes[sloppy_gazette] = sentence_sloppy_gazettes[sloppy_gazette]
+    print('Saving sloppy gazetteer dictionary', file=sys.stderr)
+    sloppy_gazetteer = defaultdict(set)
 
-        print('Saving gazetteer pickles', file=sys.stderr)
+    for gazette in gazetteer:
+        for word in gazette.split():
+            sloppy_gazetteer[word].add(gazette)
 
-        with open(os.path.join(output_dir, 'clean_gazettes_{}.pickle'.format(corpus_doc)), 'wb') as f:
-            cPickle.dump(clean_gazettes, f)
-
-        with open(os.path.join(output_dir, 'sloppy_gazettes_{}.pickle'.format(corpus_doc)), 'wb') as f:
-            cPickle.dump(sloppy_gazettes, f)
+    with open(os.path.join(output_dir, 'sloppy_gazetteer.pickle'), 'wb') as f:
+        cPickle.dump(sloppy_gazetteer, f)

2492d3f25aa1404efcf0ccb4e3de57694c5cf94a
Cristian Cardellino <crscardellino@gmail.com>
2016-06-28 09:25:21 -0300

    Moved run_classifications to one file

--- run_classifications.py
+++ run_classifications.py
@@ -6,84 +6,13 @@ import argparse
 import cPickle as pickle
 import numpy as np
 import os
-import re
 import sys
 import tensorflow as tf
 
+from mappings import LABELS_REPLACEMENT
 from scipy.sparse import csr_matrix
 from sklearn.preprocessing import normalize
 from wikipedianer.classification.mlp import MultilayerPerceptron
-
-
-def ner_label_replace(labels, mappings):
-    for label in labels:
-        if label.startswith('O'):
-            yield 'O'
-        else:
-            yield 'I'
-
-
-def ne_person_label_replace(labels, mappings):
-    for label in labels:
-        label = re.sub(r'^[BI]-', '', label)
-        if 'no_person' in mappings.get(label, set()):
-            yield 'no_person'
-        elif 'wordnet_person_100007846' in mappings.get(label, set()):
-            yield 'wordnet_person_100007846'
-        else:
-            yield 'O'
-
-
-def ne_category_label_replace(labels, mappings):
-    for label in labels:
-        label = re.sub(r'^[BI]-', '', label)
-        if 'wordnet_law_108441203' in mappings.get(label, set()):
-            yield 'wordnet_law_108441203'
-        elif 'wordnet_law_100611143' in mappings.get(label, set()):
-            yield 'wordnet_law_100611143'
-        elif 'wordnet_law_106532330' in mappings.get(label, set()):
-            yield 'wordnet_law_106532330'
-        elif 'wordnet_legal_document_106479665' in mappings.get(label, set()):
-            yield 'wordnet_legal_document_106479665'
-        elif 'wordnet_due_process_101181475' in mappings.get(label, set()):
-            yield 'wordnet_due_process_101181475'
-        elif 'wordnet_legal_code_106667792' in mappings.get(label, set()):
-            yield 'wordnet_legal_code_106667792'
-        elif 'wordnet_criminal_record_106490173' in mappings.get(label, set()):
-            yield 'wordnet_criminal_record_106490173'
-        elif 'wordnet_legal_power_105198427' in mappings.get(label, set()):
-            yield 'wordnet_legal_power_105198427'
-        elif 'wordnet_jurisdiction_108590369' in mappings.get(label, set()):
-            yield 'wordnet_jurisdiction_108590369'
-        elif 'wordnet_judiciary_108166318' in mappings.get(label, set()):
-            yield 'wordnet_judiciary_108166318'
-        elif 'wordnet_pleading_106559365' in mappings.get(label, set()):
-            yield 'wordnet_pleading_106559365'
-        elif 'wordnet_court_108329453' in mappings.get(label, set()):
-            yield 'wordnet_court_108329453'
-        elif 'wordnet_judge_110225219' in mappings.get(label, set()):
-            yield 'wordnet_judge_110225219'
-        elif 'wordnet_adjudicator_109769636' in mappings.get(label, set()):
-            yield 'wordnet_adjudicator_109769636'
-        elif 'wordnet_lawyer_110249950' in mappings.get(label, set()):
-            yield 'wordnet_lawyer_110249950'
-        elif 'wordnet_party_110402824' in mappings.get(label, set()):
-            yield 'wordnet_party_110402824'
-        else:
-            yield 'O'
-
-
-def ne_uri_label_replace(labels, mappings):
-    for label in labels:
-        yield re.sub(r"^B-", "I-", label)
-
-
-labels_replacements = dict(
-    NER=ner_label_replace,
-    NEP=ne_person_label_replace,
-    NEC=ne_category_label_replace,
-    NEU=ne_uri_label_replace
-)
 
 
 if __name__ == "__main__":
@@ -95,18 +24,19 @@ if __name__ == "__main__":
     parser.add_argument("results_dir", type=unicode)
     parser.add_argument("saves_dir", type=unicode)
     parser.add_argument("--mappings_kind", type=unicode, default='NEU', nargs='+')
+    parser.add_argument("--experiment_kind", type=unicode, default='legal')
     parser.add_argument("--learning_rate", type=float, default=0.01)
     parser.add_argument("--epochs", type=int, default=1500)
     parser.add_argument("--batch_size", type=int, default=2000)
     parser.add_argument("--loss_report", type=int, default=50)
-    parser.add_argument("--layers", type=lambda x: map(int, x.split(',')), nargs='+', default=[[12000, 9000, 7000]])
+    parser.add_argument("--layers", type=lambda x: map(int, x.split(',')), nargs='+', default=[[12000, 9000]])
 
     args = parser.parse_args()
 
     args.mappings_kind = [args.mappings_kind] if isinstance(args.mappings_kind, unicode) else args.mappings_kind
 
     for mapping_kind in args.mappings_kind:
-        if mapping_kind not in labels_replacements:
+        if mapping_kind not in LABELS_REPLACEMENT[args.experiment_kind]:
             print('Not a valid replacement {}'.format(mapping_kind), file=sys.stderr)
             sys.exit(1)
 
@@ -138,7 +68,7 @@ if __name__ == "__main__":
         print('Running experiment: {}'.format(experiment_name), file=sys.stderr)
 
         print('Replacing the labels', file=sys.stderr)
-        replacement_function = labels_replacements[mapping_kind]
+        replacement_function = LABELS_REPLACEMENT[args.experiment_kind][mapping_kind]
         experiment_labels = list(replacement_function(labels, class_mappings))
 
         print('Loading indices for train, test and validation', file=sys.stderr)

e7a5c1a1ca4dbea4911738dccebe2f89a403581d
Milagro Teruel <milagro.teruel@gmail.com>
2016-08-25 00:16:21 -0300

    Adding file to generate mapping from YAGO to LKIF

--- run_significance_test.py
+++ run_significance_test.py
@@ -1,3 +1,11 @@
+"""
+Calculate the value of the ttest for files with paired predictions.
+
+The argument is a file with the name of the pairs of predictions files. Each
+prediction file has a prediction per line. The prediction class has to be an
+integer.
+"""
+
 import argparse
 
 from scipy import stats
@@ -25,7 +33,7 @@ def significance_values(filename1, filename2):
     prediction2 = read_prediction(filename2)
 
     if len(prediction1) != len(prediction2):
-        print "Error: predictions have different sizes."
+        print 'Error: predictions have different sizes.'
         return
 
     return stats.ttest_rel(prediction1, prediction2)
@@ -35,14 +43,14 @@ def main(paired_filenames):
     filename_pairs = read_filename_pairs(paired_filenames)
     for filename1, filename2 in filename_pairs:
         t_statistic, p_value = significance_values(filename1, filename2)
-        print filename1, "vs", filename2
-        print "T Statistic", t_statistic
-        print "P Value", p_value
+        print filename1, 'vs', filename2
+        print 'T Statistic', t_statistic
+        print 'P Value', p_value
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("paired_filenames", type=unicode)
+    parser.add_argument('paired_filenames', type=unicode)
 
     args = parser.parse_args()
 

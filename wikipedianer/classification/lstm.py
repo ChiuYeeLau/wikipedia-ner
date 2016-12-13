f177f0f1294f4e81441bb345d8279cbb6205a764
Milagro Teruel <milagro.teruel@gmail.com>
2016-12-12 22:54:46 -0300

    Changing lstm to tensorflow

--- wikipedianer/classification/lstm.py
+++ wikipedianer/classification/lstm.py
@@ -1,40 +1,146 @@
 """LSTM Keras classifier."""
 
 import numpy
+import tensorflow as tf
+
 
 from base import BaseClassifier
-from keras.models import Sequential
-from keras.layers.recurrent import LSTM
-from keras.regularizers import l2
+from tensorflow.python.ops import rnn, rnn_cell
 
 
 class LSTMClassifier(object):
-    """LSTM Keras classifier."""
-    def __init__(self, input_dim, lstm_state_size=100):
+    """LSTM tensorflow classifier."""
+    def __init__(self):
+        """Creates a LSTM classifier.
+
+        Args:
+            input_dim: size of each element. (Number of features)
+            input_length: size of each sequence.
+            lstm_state_size:
+        """
         # Build the network
-        self.model = Sequential()
-        self.model.add(LSTM(
-            lstm_state_size, input_dim=input_dim, W_regularizer=l2(0.01),
-            U_regularizer=l2(0.01), b_regularizer=l2(0.01),
-            dropout_W=0.1, dropout_U=0.1, return_sequences=True))
-        self.model.compile(loss='binary_crossentropy', optimizer='adam',
-                      metrics=['accuracy'])
+        self.learning_rate = 0.001
+        self.training_iters = 100
+        self.batch_size = 128
+        self.display_step = 10
+
+        self.session = None
 
     def fit(self, train_x, train_y):
         """Trains the classifier"""
-        self.model.fit(train_x, train_y, nb_epoch=100, batch_size=1, verbose=2)
+        n_input = train_x.shape[-1]  # Number of features in each instance
+        n_steps = train_x.shape[1]  # Number of instances in each sequence
+        n_hidden = 128  # hidden layer num of features
+        n_classes = train_y.shape[1]  # Total number of classes
+
+        # tf Graph input
+        self.x_placeholder = tf.placeholder("float", [None, n_steps, n_input])
+        self.y_placeholder = tf.placeholder("float", [None, n_classes])
+
+        # Define weights
+        weights = {
+            'out': tf.Variable(tf.random_normal([n_hidden, n_classes]))
+        }
+        biases = {
+            'out': tf.Variable(tf.random_normal([n_classes]))
+        }
+
+        def RNN(x, weights, biases):
+            # Prepare data shape to match `rnn` function requirements
+            # Current data input shape: (batch_size, n_steps, n_input)
+            # Required shape: 'n_steps' tensors list of shape (batch_size, n_input)
+
+            # Permuting batch_size and n_steps
+            x = tf.transpose(x, [1, 0, 2])
+            # Reshaping to (n_steps*batch_size, n_input)
+            x = tf.reshape(x, [-1, n_input])
+            # Split to get a list of 'n_steps' tensors of shape (batch_size, n_input)
+            x = tf.split(0, n_steps, x)
+
+            # Define a lstm cell with tensorflow
+            lstm_cell = rnn_cell.BasicLSTMCell(n_hidden, forget_bias=1.0)
+
+            # Get lstm cell output
+            outputs, states = rnn.rnn(lstm_cell, x, dtype=tf.float32)
+
+            # Linear activation, using rnn inner loop last output
+            return tf.matmul(outputs[-1], weights['out']) + biases['out']
+
+        self.prediction = RNN(self.x_placeholder, weights, biases)
+
+        # Define loss and optimizer
+        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+            self.prediction, self.y_placeholder))
+        optimizer = tf.train.AdamOptimizer(
+            learning_rate=self.learning_rate).minimize(cost)
+
+        # Evaluate model
+        correct_pred = tf.equal(tf.argmax(self.prediction, 1), tf.argmax(self.y_placeholder, 1))
+        self.accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+        # Initializing the variables
+        init = tf.initialize_all_variables()
+
+        self.session = tf.Session()
+        self.session.run(init)
+        # Keep training until reach max iterations
+        for iter, batch_start in enumerate(
+                range(0, self.training_iters, self.batch_size)):
+            batch_x = train_x[batch_start:batch_start+self.batch_size]
+            batch_y = train_y[batch_start:batch_start+self.batch_size]
+            # Reshape data to get 28 seq of 28 elements
+            # batch_x = batch_x.reshape((self.batch_size, n_steps, n_input))
+            # Run optimization op (backprop)
+            self.session.run(optimizer, feed_dict={
+                self.x_placeholder: batch_x,
+                self.y_placeholder: batch_y})
+            if iter % self.display_step == 0:
+                # Calculate batch accuracy
+                acc = self.session.run(self.accuracy, feed_dict={
+                    self.x_placeholder: batch_x,
+                    self.y_placeholder: batch_y
+                })
+                # Calculate batch loss
+                loss = self.session.run(cost, feed_dict={
+                    self.x_placeholder: batch_x,
+                    self.y_placeholder: batch_y})
+                print ("Iter {}, Minibatch Loss={:.6f}, Training Accuracy= "
+                       "{:.5f}").format(iter, loss, acc)
+        print "Optimization Finished!"
 
     def predict(self, train_x):
-        self.model.predict(train_x)
+        return self.prediction.eval(
+            session=self.session, feed_dict={self.x_placeholder: train_x})
+
+    def __del__(self):
+        self.session.close()
 
 
 class SequentialPipeline(BaseClassifier):
     """Pipeline for sequential classifier for distributed datasets."""
     def __init__(self, dataset, labels, train_indices, test_indices,
                  validation_indices):
-        super(LSTMClassifier, self).__init__(dataset, labels, train_indices,
+        super(SequentialPipeline, self).__init__(dataset, labels, train_indices,
                                              test_indices, validation_indices)
-        self.classifier = LSTMClassifier(self.train_dataset.shape[1])
+        self.classifier = LSTMClassifier()
 
-    def train(self):
+    def train(self, sequence_max_length):
+        # Pad missing sequences with zero
+        self.pad_sequences(self.train_dataset, sequence_max_length)
         self.classifier.fit(self.train_dataset, self.train_labels)
+
+    @staticmethod
+    def pad_sequences(x_matrix, sequence_max_length=10):
+        # Get the number of dimensions
+        if len(x_matrix[0].shape) == 2:
+            pad_width = lambda shape: [(0, sequence_max_length - shape), (0, 0)]
+        else:
+            pad_width = lambda shape: (0, sequence_max_length - shape)
+        for index, sequence in enumerate(x_matrix):
+            if sequence.shape[0] > sequence_max_length:
+                x_matrix[index] = sequence[:sequence_max_length]
+            elif sequence.shape[0] < sequence_max_length:
+                sequence = numpy.pad(sequence, pad_width(sequence.shape[0]),
+                                     mode='constant')
+                x_matrix[index] = sequence
+        return numpy.array(x_matrix)
--- wikipedianer/classification/test_lstm.py
+++ wikipedianer/classification/test_lstm.py
@@ -1,34 +1,70 @@
-"""Simple test suite for the LSTMClassifier."""
+"""Simple test suite for the LSTMClassifier.
+"""
 
 import unittest
 import numpy
 
-from lstm import LSTMClassifier
+from lstm import LSTMClassifier, SequentialPipeline
 
-class LSTMClassifierTest(unittest.TestCase):
-    """Simple test suite for the LSTMClassifier."""
+
+class SequentialPipelineTest(unittest.TestCase):
+
     def setUp(self):
-        self.train_x = [
+        self.x_matrix = [
             # Sequence 1
             numpy.array([[1, 2, 1], [2, 1, 0], [2, 3, 1]]),
             # Sequence 2
             numpy.array([[1, 0, 0], [0, 0, 0]]),
             # Sequence 3
-            numpy.array([[1, 0, 0], [2, 1, 0], [2, 3, 1]]),
+            numpy.array([[1, 0, 0]]),
         ]
-        self.train_y = [
-            [1, 2, 2],
-            [1, 0],
-            [1, 2, 2]
+        self.y_vector = [
+            numpy.array([1, 2, 2]),
+            numpy.array([1, 0]),
+            numpy.array([1])
         ]
-        self.classifier = LSTMClassifier(input_dim=3, lstm_state_size=2)
+
+    def test_padding(self):
+        result = SequentialPipeline.pad_sequences(self.x_matrix,
+                                                  sequence_max_length=4)
+        self.assertEqual((3, 4, 3), result.shape)
+        self.assertEqual([0.0, 0.0, 0.0], result[1,2,:].tolist())
+
+    def test_padding_smaller_shape(self):
+        result = SequentialPipeline.pad_sequences(self.x_matrix,
+                                                  sequence_max_length=2)
+        self.assertEqual((3, 2, 3), result.shape)
+
+    def test_padding_label_vector(self):
+        result = SequentialPipeline.pad_sequences(self.y_vector,
+                                                  sequence_max_length=3)
+        self.assertEqual((3, 3), result.shape)
+
+
+class LSTMClassifierTest(unittest.TestCase):
+    """Simple test suite for the LSTMClassifier."""
+    def setUp(self):
+        self.train_x = SequentialPipeline.pad_sequences([
+            # Sequence 1
+            numpy.array([[1, 2, 1], [2, 1, 0], [2, 3, 1]]),
+            # Sequence 2
+            numpy.array([[1, 0, 0], [0, 0, 0]]),
+            # Sequence 3
+            numpy.array([[1, 0, 0]]),
+        ], sequence_max_length=3)
+        self.train_y = SequentialPipeline.pad_sequences([
+            numpy.array([1, 2, 2]),
+            numpy.array([1, 0]),
+            numpy.array([1])
+        ], sequence_max_length=3)
 
     def test_simple_case(self):
+
+        self.classifier = LSTMClassifier()
         self.classifier.fit(self.train_x, self.train_y)
         predictions = self.classifier.predict(self.train_x)
         self.assertEqual(3, len(predictions))
         self.assertEqual((3,), predictions[0].shape)
-
 
 
 if __name__ == '__main__':

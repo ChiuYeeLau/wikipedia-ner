f95f49c7692664f2a7f01ad8966d8be5da715427
Milagro <milagro.teruel@gmail.com>
2017-01-05 17:11:51 -0300

    Adding heuristic classifier

--- run_classifications_nel.py
+++ run_classifications_nel.py
@@ -21,6 +21,7 @@ import pandas
 logging.basicConfig(level=logging.INFO)
 
 from wikipedianer.classification import double_step_classifier
+from wikipedianer.classification import heuristic_classifier
 from wikipedianer.classification import logistic_regression
 from wikipedianer.classification import random_classifier
 from wikipedianer.dataset import HandcraftedFeaturesDataset
@@ -49,9 +50,6 @@ def read_arguments():
     parser.add_argument('--features_filename', type=str, default=None,
                         help='Path of file with the filtered features. Only '
                              'for heuristic classifier.')
-    parser.add_argument('--entities_filename', type=str, default=None,
-                        help='Path of jsonlines file with the entity labels. '
-                             'Only for heuristic classifier.')
     parser.add_argument('--evaluate-only', action='store_true',
                         help='Do not train the classifier, only perform'
                              'evaluation')
@@ -114,37 +112,8 @@ def main():
             dropout_ratio=args.dropout_ratio, num_layers=args.num_layers)
 
     elif args.classifier == 'heuristic':
-        # Read the entities
-        entities_database = defaultdict(set)
-        with jsonlines.open(args.entities_filename) as reader:
-            for entity in reader:
-                uri = entity['uri']
-                labels = entity['labels']
-                for label in labels:
-                    for word in label.split():
-                        entities_database[word].add(uri)
-
-        # Read the features
-        with open(args.features_filename, 'rb') as feature_file:
-            features = pickle.load(feature_file)
-            # Get indices for each current token
-        token_features = {
-            index: x.split('token:current=')[1]
-            for index, x in enumerate(features)
-            if x.startswith('token:current')}
-        factory = double_step_classifier.HeuristicClassifierFactory(
-            entities_database, token_features)
-        classifier.train(classifier_factory=factory)
-        classifier.save_to_file(results_dirname=args.results_dirname)
-        metrics = classifier.evaluate()
-
-        print('Accuracy {}'.format(metrics[0]))
-        print('Classes {}'.format(classifier.classes[1]))
-        print('Precision {}'.format(metrics[1]))
-        print('Recall {}'.format(metrics[2]))
-        print('F1 Score {}'.format(metrics[3]))
-        classifier.close_open_sessions()
-
+        factory = heuristic_classifier.HeuristicClassifierFactory(
+            args.features_filename)
     elif args.classifier == 'lr':
         factory = logistic_regression.LRClassifierFactory(
             save_models=True, results_save_path=args.results_dirname)
@@ -161,7 +130,6 @@ def main():
     logging.info('Starting evaluation')
     save_evaluation_results(classifier, args.results_dirname, factory)
     logging.info('All operations completed')
-
 
 
 if __name__ == '__main__':
--- wikipedianer/classification/heuristic_classifier.py
+++ wikipedianer/classification/heuristic_classifier.py
@@ -1,66 +1,98 @@
 """Heuristic classifier for Named Entity Linking using partial matches"""
-
+import logging
 import numpy
-import pandas
+import pickle
 import random
 
+from collections import defaultdict
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from .base import BaseClassifier
+from .double_step_classifier import ClassifierFactory
 
 
-class HeuristicClassifier(object):
+class HeuristicClassifierFactory(ClassifierFactory):
+    def __init__(self, features_filename):
+        # Read the features
+        with open(features_filename, 'rb') as feature_file:
+            features = pickle.load(feature_file)
+            # Get indices for each current token
+        self.token_features = [
+            index for index, x in enumerate(features)
+            if x.startswith('token:current')]
+        self.prev_features = [
+            index for index, x in enumerate(features)
+            if x.startswith('token:prev')]
+        self.next_features = [
+            index for index, x in enumerate(features)
+            if x.startswith('token:next')]
+        self.total_features = len(features)
+
+    def get_classifier(self, dataset, experiment_name):
+        assert self.total_features == dataset.datasets['train'].data.shape[1]
+        return HeuristicClassifier(dataset, self.token_features,
+                                   self.prev_features, self.next_features)
+
+
+class HeuristicClassifier(BaseClassifier):
 
     DEFAULT_LABEL = 'O'
 
-    def __init__(self, dataset, entities, features):
+    def __init__(self, dataset, token_features, prev_features, next_features):
+        """
+
+        :param dataset:
+        :param entities:
+        :param features: A dictionary from indexes in the feature array to
+         tokens. Only indices of features indicating the current token are used.
+        """
+        super(HeuristicClassifier, self).__init__()
         self.dataset = dataset
-        self.entities = entities
-        self.features_map = features
-        # We ensure keys have always the same order
-        self.features_keys = sorted(list(self.features_map.keys()))
-        self.mask = numpy.zeros((len(self.features_keys),))
-        self.default_index = None
-        self._add_O_class()
-        self.test_results = pandas.DataFrame(
-            columns=['accuracy', 'class', 'precision', 'recall', 'fscore'])
+        # A map from each token index in the training dataset to the
+        # list of labels it has been tagged with.
+        self.token_to_label_map = defaultdict(list)
+        # A map from each label to a set of unigrams, bigrams and trigrams
+        # (ordered) extracted from the label mentions in the train dataset.
+        self.n_gram_map = defaultdict(list)
 
-    def _add_O_class(self):
-        index = numpy.where(self.dataset.classes[1] ==
-                                         self.DEFAULT_LABEL)[0]
-        if index.shape[0] == 0:  # Default label not in classes
-            self.default_index = self.dataset.classes[1].shape[0]
-            self.dataset.classes = (
-                self.dataset.classes[0],
-                numpy.append(self.dataset.classes[1], self.DEFAULT_LABEL))
-        else:
-            self.default_index = index[0]
+        self.token_features = token_features
+        self.next_features = next_features
+        self.prev_features = prev_features
 
-    def get_token(self, instance):
-        feature_index = numpy.where(
-            instance[self.mask, self.features_keys] == 1)[1]
+    def get_token_index(self, instance, possible_values):
+        feature_index = instance[:, possible_values].nonzero()[1]
         if feature_index.shape[0] == 0:
             return None
-        return self.features_map[self.features_keys[feature_index]]
-
-    def get_numeric_label(self, label):
-        label_index = numpy.where(self.dataset.classes[1] == label)[0]
-        if label_index.shape[0] == 0:
-            return self.default_index
-        return label_index[0]
+        return feature_index[0]
 
     def predict(self, x_matrix):
         predictions = []
+        random_predictions = 0
         for instance in x_matrix:
-            token = self.get_token(instance)
-            prediction = self.entities.get(token)  # if exists, is a set
-            if prediction is None or len(prediction) == 0:
-                prediction = 'O'
-            else:
-                prediction = 'I-' + random.sample(prediction, 1)[0]
-            predictions.append(self.get_numeric_label(prediction))
+            token_index = self.get_token_index(instance, self.token_features)
+            possible_labels = self.token_to_label_map.get(token_index, [])
+            if len(possible_labels) is 0:
+                predictions.append(random.randrange(
+                    0, self.dataset.classes[1].shape[0]))
+                random_predictions += 1
+                continue
+            prev_index = self.get_token_index(instance, self.prev_features)
+            next_index = self.get_token_index(instance, self.next_features)
+            max_score = 0
+            selected_label = None
+            for label in possible_labels:
+                ngrams = self.get_ngram_set(token_index, prev_index, next_index)
+                for possible_ngrams in self.n_gram_map.get(label, []):
+                    label_score = len(ngrams.intersection(possible_ngrams))
+                    if max_score < label_score:
+                        max_score = label_score
+                        selected_label = label
+            predictions.append(selected_label)
         return numpy.array(predictions)
 
-    def evaluate(self):
-        # Get test accuracy
+    def evaluate(self, *args, **kwargs):
+        if len(self.token_to_label_map) == 0:
+            self.train()
+
         y_pred = self.predict(self.dataset.datasets['test'].data)
         y_true = self.dataset.dataset_labels('test', 1)
         assert y_pred.shape[0] == y_true.shape[0]
@@ -73,16 +105,33 @@ class HeuristicClassifier(object):
         fscore = f1_score(y_true, y_pred, labels=labels, average=None)
         return accuracy, precision, recall, fscore, y_true, y_pred
 
+    def get_ngram_set(self, token, prev, next_):
+        return set([(token, ), (prev, token), (token, next_),
+                    (prev, token, next_)])
+
     def train(self, save_layers=False):
+        # Count all instances
+        no_token_instances = 0
+        for index, instance in enumerate(self.dataset.datasets['train'].data):
+            token_index = self.get_token_index(instance, self.token_features)
+            if not token_index:
+                no_token_instances += 1
+                continue
+            prev_index = self.get_token_index(instance, self.prev_features)
+            next_index = self.get_token_index(instance, self.next_features)
+
+            label = self.dataset.datasets['train'].labels[index][1]
+            self.token_to_label_map[token_index].append(label)
+            self.n_gram_map[label].append(self.get_ngram_set(
+                token_index, prev_index, next_index))
+        logging.info('Training completed.')
+        logging.info('No token instances: {0:.2f}'.format(
+            no_token_instances/float(self.dataset.num_examples('train'))))
+        logging.info('Average labels per token: {}'.format(
+            numpy.mean([len(x) for x in self.token_to_label_map.values()])))
+
         accuracy, precision, recall, fscore, y_true, y_pred = self.evaluate()
 
-        self.test_results = self.test_results.append({'accuracy': accuracy},
-                                                     ignore_index=True)
-        for cls_idx, cls in enumerate(self.dataset.classes[1]):
-            self.test_results = self.test_results.append({
-                'class': cls,
-                'precision': precision[cls_idx],
-                'recall': recall[cls_idx],
-                'fscore': fscore[cls_idx]
-            }, ignore_index=True)
+        self.add_test_results(accuracy, precision, recall, fscore,
+                              classes=self.dataset.classes[1], y_true=y_true)
 
